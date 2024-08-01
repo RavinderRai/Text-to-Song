7a756814d3b16a1061b03928529dc1247e6f73e1
Ravi Rai <Ravi.B.Rai@outlook.com>
2024-07-31 23:20:42 -0300

    created a blog post in a notebook about the project

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
+import os
+from dotenv import load_dotenv
+import replicate
+from openai import OpenAI
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from transformers import pipeline
-from openai import OpenAI
-import os
-import replicate
-from dotenv import load_dotenv
 
 """useful links:
 https://www.youtube.com/watch?v=SbRC81kZBkE
@@ -23,32 +22,6 @@ openai_api_key = os.getenv('OPENAI_API_KEY')
 app.mount("/static", StaticFiles(directory = "static"), name = "static")
 
 templates = Jinja2Templates(directory = "templates")
-
-def get_openai_lyrics(prompt, temperature):
-    client = OpenAI()
-
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "system",
-                "content": "You are a music lyrics writer and your task is to write lyrics of \
-                music under 30 words based on user's prompt. Just return the lyrics and nothing else.",
-            },
-            {
-                "role": "user",
-                "content": prompt
-            }
-        ],
-        temperature=temperature,
-        max_tokens=50,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
-    output = response.choices[0].message.content
-    cleaned_output = output.replace("\n", " ")
-    return cleaned_output
 
 @app.get("/")
 async def index(request: Request):
